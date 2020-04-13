4a2f7a88691d7d8951d4a59b831350677da45e11
Elaine <elaine.noh@gmail.com>
2020-04-12 21:59:03 -0700

    add mod packages

---
+++ Minecraft Modding/StudioGhibliMod/src/main/java/com/jihyunniee/studioghibli/StudioGhibli.java
@@ -0,0 +1,35 @@
+package com.jihyunniee.studioghibli;
+
+import com.jihyunniee.studioghibli.util.RegistryHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("sgworld")
+public class StudioGhibli
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "sgworld";
+
+    public StudioGhibli() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        RegistryHandler.init();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
